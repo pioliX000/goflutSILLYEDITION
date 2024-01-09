66696e3e638fb08909d4953719bf372d60dd2b5f
pioliX000 <65280877+pioliX000@users.noreply.github.com>
2024-01-09 19:09:15 +0000

    Added bouncing image (it sucks)

--- main.go
+++ main.go
@@ -65,9 +65,9 @@ func drawCircle(x, y, rad, r, g, b int, conn net.Conn) {
 }
 
 func drawRect(x, y, w, h, r, g, b int, conn net.Conn) {
-	for i := x; i < x+w; i++ {
-		for j := y; j < y+h; j++ {
-			writePixel(i, j, r, g, b, 255, conn)
+	for i := x; i < x+h; i++ {
+		for j := y; j < y+w; j++ {
+			writePixel(j, i, r, g, b, 255, conn)
 		}
 	}
 }
@@ -80,13 +80,43 @@ func bouncingBall(x, y, xvel, yvel, rad, r, g, b int, conn net.Conn) {
 		if x > 1280 || x < 0 {
 			xvel *= -1
 		}
-		if y > 720 || y < 0{
+		if y > 720 || y < 0 {
 			yvel *= -1
 		}
 
 		drawCircle(x-xvel, y-yvel, rad, 255, 255, 255, conn)
 		drawCircle(x, y, rad, r, g, b, conn)
 	}
+}
+
+func bouncingImage(x, y, xvel, yvel int, path string, conn net.Conn, threads int) {
+    f, err := os.Open(path)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Can't open image womp womp")
+        os.Exit(1)
+    }
+    
+    img,  _, err  := image.Decode(f)
+    bounds := img.Bounds()
+    imgWidth := bounds.Max.X
+    imgHeight := bounds.Max.Y
+
+    for true {
+        x += xvel
+        y += yvel
+        canvasSize := getSize(conn)
+
+        if x > canvasSize.width || x < 0 {
+            xvel *= -1
+        }
+        if y > canvasSize.height || y < 0{
+            yvel *= -1
+        }
+
+        drawImage(path, x, y, threads, 0.1, conn)
+        time.Sleep(50*time.Millisecond)
+        drawRect(x-xvel, y-yvel, int(float64(imgWidth) * 0.1), int(float64(imgHeight) * 0.1), 255, 255, 255, conn)
+    }
 }
 func makeChunks(threadsCount int, chunkWidth int, chunkHeight int, chunkScale float64) []chunk {
 
@@ -170,7 +200,7 @@ func main() {
 
 	var host      *string = flag.String("host", "", "The PixelFlut server host ip or domain.")
 	var port      *string = flag.String("port", "", "The port of the PixelFlut server.")
-	// var imagePath *string = flag.String("image", "", "The path to the image to draw.")
+	var imagePath *string = flag.String("image", "", "The path to the image to draw.")
 	var threads      *int = flag.Int("threads", 1, "Number of threads to use.")
 
 	required := []string{"host", "port"}
@@ -198,7 +228,7 @@ func main() {
 	}
 	defer conn.Close()
 
-	bouncingBall(0, 0, 1, 2, 8, 100, 100, 100, conn)
+	bouncingImage(0, 0, 1, 1, *imagePath, conn, *threads)
 
 	// err = drawImage(*imagePath, 800, 0, *threads, 0.5, conn)
 	// if err != nil {
